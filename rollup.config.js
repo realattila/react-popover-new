8e1be13309bd30dc913a2fac02810db684a8b22a
attila <98atila@gmail.com>
2022-02-10 13:47:40 +0330

    update disk folder

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,8 @@ export default {
   input: "src/index.tsx",
   output: [
     {
-      file: pkg.main,
+      file: "./dist/" + pkg.main,
+
       format: "cjs",
       exports: "named",
       sourcemap: true,
